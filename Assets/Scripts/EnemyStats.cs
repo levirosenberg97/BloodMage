2f8271baca53bae6818e9c38459b63b9cc08c752
Levi Rosenberg <levirosenberg97@gmail.com>
2019-06-10 08:42:39 -0700

    first commit

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EnemyStats : MonoBehaviour
+{
+    public bool selectable;
+
+    public int health;
+
+    public Slider healthSlider;
+
+    public int strength;
+    public int defense;
+    public int speed;
+}
---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public PlayerStats player;
+
+    public void PhysicalAttack()
+    {
+        EnemyStats enemy = player.target;
+
+        enemy.health -= player.strength;
+    }
+
+    public void MagicAttack()
+    {
+        EnemyStats enemy = player.target;
+
+        enemy.health  -= player.spellPower;
+        player.health -= player.spellPower / 2;
+
+        enemy.healthSlider.value  = enemy.health;
+        player.healthSlider.value = player.health;
+    }
+
+    public void DefendYourself()
+    {
+        EnemyStats enemy = player.target;
+    }
+
+    void MakeTargetable()
+    {
+        var enemies = FindObjectsOfType<EnemyStats>();
+        foreach (EnemyStats enemy in enemies)
+        {
+            enemy.selectable = true;
+        }
+    }
+}
